3b10dd80560693830a8589010cd6cf5ccab06fae
Aaron Stannard <aaron@aaronstannard.com>
2011-10-26 22:08:39 -0700

    Added generators for Guids and strings

---
+++ Faker/Generators/Guids.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Faker.Generators
+{
+    /// <summary>
+    /// Generator for creating new Guids
+    /// </summary>
+    public static class Guids
+    {
+        /// <summary>
+        /// Returns a new Guid
+        /// </summary>
+        /// <returns>A Guid</returns>
+        public static Guid GetGuid()
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
---
+++ Faker/Generators/Strings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Faker.Generators
+{
+    /// <summary>
+    /// Generator class used for creating random stings
+    /// </summary>
+    public static class Strings
+    {
+        private static readonly Random R = new Random();
+
+        /// <summary>
+        /// Generates a random string of the specified length
+        /// </summary>
+        /// <param name="length">The length of the random string</param>
+        /// <returns>A string</returns>
+        public static string GenerateString(int length = 20)
+        {
+            var sb = new StringBuilder();
+
+            for(var i = 0; i < length; i++)
+            {
+                var next = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * R.NextDouble() + 65)));
+                sb.Append(next);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
