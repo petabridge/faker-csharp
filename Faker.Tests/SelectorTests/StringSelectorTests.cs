fe8eec79dd3c717cf949ad703ab72a2402b182d7
Aaron Stannard <aaron@aaronstannard.com>
2011-12-18 23:59:43 -0800

    Created a unit test for actually testing the values of the the FullNameSelector

--- Faker.Tests/SelectorTests/StringSelectorTests.cs
+++ Faker.Tests/SelectorTests/StringSelectorTests.cs
@@ -39,7 +39,25 @@ namespace Faker.Tests.SelectorTests
         [Test(Description = "Tests to see if all of our field values are properly injected...")]
         public void Full_Name_Variations_All_Injected()
         {
-            
+            var nameSelector = new FullNameSelector();
+            var fullNameClass = new FullNameTestClass();
+
+            //Iterate over all of the properties in the fullNameClass object...
+            foreach (var property in fullNameClass.GetType().GetProperties())
+            {
+                //Inject the value into the property
+                nameSelector.Generate(fullNameClass, property);
+            }
+
+            //Iterate over all of the properties again
+            foreach(var property in fullNameClass.GetType().GetProperties())
+            {
+                var fieldValue = property.GetValue(fullNameClass, null) as string;
+
+                Assert.IsNotNullOrEmpty(fieldValue);
+                Assert.IsAssignableFrom<string>(fieldValue, "Should be type of string...");
+                Assert.That(fieldValue.Length > 0);
+            }
         }
     }
 }
