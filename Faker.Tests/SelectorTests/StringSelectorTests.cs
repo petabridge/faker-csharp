042c9c0a9e93f214e5ffa7e46144ee6517459314
Aaron Stannard <aaron@aaronstannard.com>
2011-12-18 23:50:59 -0800

    Wrote first test for validating that the FullNameSelector works - needed to declare the regex strings for identifying special fields as format strings in order for the Regex parser doesn't explode into flames.

--- Faker.Tests/SelectorTests/StringSelectorTests.cs
+++ Faker.Tests/SelectorTests/StringSelectorTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Faker.Selectors;
 using NUnit.Framework;
 
 namespace Faker.Tests.SelectorTests
@@ -9,5 +10,36 @@ namespace Faker.Tests.SelectorTests
     [TestFixture(Description = "Ensures that our string selectors inject valid results")]
     public class StringSelectorTests
     {
+        private class FullNameTestClass
+        {
+            public string fullname { get; set; }
+            public string fullName { get; set; }
+            public string Fullname { get; set; }
+            public string FullName { get; set; }
+            public string Full_name { get; set; }
+            public string Full_Name { get; set; }
+            public string full_name { get; set; }
+            public string full_Name { get; set; }
+        }
+
+        [Test(Description = "Tests to see if our regex can match all of the variations of the FullName field")]
+        public void Full_Name_Variations_All_Match()
+        {
+            var nameSelector = new FullNameSelector();
+            var fullNameClass = new FullNameTestClass();
+
+            //Iterate over all of the properties in the fullNameClass object...
+            foreach(var property in fullNameClass.GetType().GetProperties())
+            {
+                var nameSelectorResult = nameSelector.CanBind(property);
+                Assert.IsTrue(nameSelectorResult, string.Format("{0} should have been a valid match", property.Name));
+            }
+        }
+
+        [Test(Description = "Tests to see if all of our field values are properly injected...")]
+        public void Full_Name_Variations_All_Injected()
+        {
+            
+        }
     }
 }
