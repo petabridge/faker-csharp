f2b2155e24b3b2e81ccc549251893d5ef92fd972
Aaron Stannard <aaron@aaronstannard.com>
2012-01-07 00:21:08 -0800

    Added additional unit tests to ensure that do we appropriate things like strip domains when expected

--- Faker.Tests/GeneratorTests/EmailAddressGeneratorTests.cs
+++ Faker.Tests/GeneratorTests/EmailAddressGeneratorTests.cs
@@ -12,23 +12,43 @@ namespace Faker.Tests.GeneratorTests
     public class EmailAddressGeneratorTests
     {
         public const string ValidEmailRegex = @"(\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b)";
+        public const string MajorDomainRegex = @"(.com)|(.net)|(.org)|(.edu)";
 
-        public Regex _r = new Regex(ValidEmailRegex, RegexOptions.IgnoreCase);
+        public Regex _valid_email_regex = new Regex(ValidEmailRegex, RegexOptions.IgnoreCase);
+        public Regex _major_domain_regex = new Regex(MajorDomainRegex);
 
         [Test(Description = "Can we generate valid emails using our fully-randomized method?")]
         public void Can_Generate_Valid_Random_Emails()
         {
-            var email = EmailAddresses.Generate();
+            for(var i = 0; i < 200; i++)
+            {
+                var email = EmailAddresses.Generate();
 
-            Assert.IsTrue(_r.IsMatch(email), "Expected a valid email address");
+                Assert.IsTrue(_valid_email_regex.IsMatch(email), "Expected a valid email address");
+            }
+            
         }
 
         [Test(Description = "Can we generate valid emails using our 'humanized' method?")]
         public void Can_Generate_Valid_Human_Emails()
         {
-            var email = EmailAddresses.Human();
+            for (var i = 0; i < 200; i++)
+            {
+                var email = EmailAddresses.Human();
 
-            Assert.IsTrue(_r.IsMatch(email), "Expected a valid email address");
+                Assert.IsTrue(_valid_email_regex.IsMatch(email), "Expected a valid email address");
+            }
+        }
+
+        [Test(Description = "We should only include major domain extensions (.com, .net, .org, and .edu) for randomly generated addresses when the flag is set correctly.")]
+        public void Should_Include_Only_Major_Domain_Extensions_When_Flag_Is_Set_For_Random_Addresses()
+        {
+            var email = EmailAddresses.Generate(true);
+
+            var extensionPos = email.LastIndexOf('.');
+            var extension = email.Substring(extensionPos, email.Length - extensionPos);
+
+            Assert.IsTrue(_major_domain_regex.IsMatch(extension));
         }
     }
 }
