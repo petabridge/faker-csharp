1f657f10d5d437801ef63002a4c3b57a6194df75
Aaron Stannard <aaron@aaronstannard.com>
2012-01-06 22:05:09 -0800

    Moved the type table back to its original position

--- Faker/TypeTable.cs
+++ Faker/TypeTable.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Faker.Selectors;
 
-namespace Faker.Selectors
+namespace Faker
 {
     public enum SelectorPosition
     {
