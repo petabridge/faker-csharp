f3e2c9418fb096f416ffbfe239a879c682191db4
Aaron Stannard <aaron@aaronstannard.com>
2012-01-07 01:20:01 -0800

    Created an IntSelector for integers

--- Faker/Selectors/NumberSelectors.cs
+++ Faker/Selectors/NumberSelectors.cs
@@ -7,6 +7,9 @@ using Faker.Generators;
 
 namespace Faker.Selectors
 {
+    /// <summary>
+    /// Injects doubles into double fields within a range
+    /// </summary>
     public sealed class DoubleSelector : TypeSelectorBase<double>
     {
         public DoubleSelector()
@@ -23,6 +26,27 @@ namespace Faker.Selectors
         public override void Generate(object targetObject, PropertyInfo property)
         {
             property.SetValue(targetObject, Numbers.Double(MinSize, MaxSize), null);
+        }
+
+        #endregion
+    }
+
+    public sealed class IntSelector : TypeSelectorBase<int>
+    {
+        public IntSelector()
+        {
+            MinSize = Int32.MinValue;
+            MaxSize = Int32.MaxValue;
+        }
+
+        public int MaxSize { get; set; }
+        public int MinSize { get; set; }
+
+        #region Overrides of TypeSelectorBase<int>
+
+        public override void Generate(object targetObject, PropertyInfo property)
+        {
+            property.SetValue(targetObject, Numbers.Int(MinSize, MaxSize), null);
         }
 
         #endregion
