f250844f1d7ff474319cc771663afbe2cf42eb87
Aaron Stannard <aaron@aaronstannard.com>
2012-01-07 18:59:52 -0800

    Added a Guid selector to the mix

---
+++ Faker/Selectors/GuidSelector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Faker.Generators;
+
+namespace Faker.Selectors
+{
+    /// <summary>
+    /// Type selector used for generating Guid values
+    /// </summary>
+    public sealed class GuidSelector : TypeSelectorBase<Guid>
+    {
+        #region Overrides of TypeSelectorBase<Guid>
+
+        public override void Generate(object targetObject, PropertyInfo property)
+        {
+            property.SetValue(targetObject, Guids.GetGuid(), null);
+        }
+
+        #endregion
+    }
+}
