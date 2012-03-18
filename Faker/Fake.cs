520cf6e5a1bd228e019b77e761888f3115e6e6b6
Aaron Stannard <aaron@aaronstannard.com>
2012-03-18 12:37:29 -0700

    Implemented the Fake class, but there is still work to be done on Matcher.

--- Faker/Fake.cs
+++ Faker/Fake.cs
@@ -12,11 +12,14 @@ namespace Faker
     /// </summary>
     public class Fake<T> where T : new()
     {
-        private IList<ITypeSelector> _selectors;
+        /// <summary>
+        /// Engine used to power our fakes
+        /// </summary>
+        private readonly Matcher _matcher;
 
         public Fake()
         {
-            _selectors = new List<ITypeSelector>();
+            _matcher = new Matcher();
         }
 
         /// <summary>
@@ -25,7 +28,14 @@ namespace Faker
         /// <returns>A populated instance of a given class</returns>
         T Generate()
         {
-            throw new NotImplementedException();
+            //create a new instance of the type we want to Fake
+            var instance = Activator.CreateInstance<T>();
+
+            //Match all of the properties of the object and come up with the most reasonable guess we can as to the type of data needed
+            _matcher.Match(instance);
+
+            //Return the instance once matching is complete
+            return instance; 
         }
 
         /// <summary>
@@ -34,7 +44,17 @@ namespace Faker
         /// <returns>A list of populated instances with length [count] of a given class</returns>
         IList<T> Generate(int count)
         {
-            throw new NotImplementedException();
+            //Create a list to hold all of the fakes we want to return back to the caller
+            var items = new List<T>();
+
+            //Build the list of objects
+            for(var i = 0; i < count; i++)
+            {
+                items.Add(this.Generate());
+            }
+
+            //Return the list to the caller
+            return items;
         }
     }
 }
