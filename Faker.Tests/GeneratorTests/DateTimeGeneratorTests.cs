dfb5cd79d08181407874a5e62d431865e0df71fe
Aaronontheweb <aaron@aaronstannard.com>
2012-07-11 23:07:51 -0700

    Added unit tests to valdiate that our DateTimeOffset generator works as expected

--- Faker.Tests/GeneratorTests/DateTimeGeneratorTests.cs
+++ Faker.Tests/GeneratorTests/DateTimeGeneratorTests.cs
@@ -31,7 +31,7 @@ namespace Faker.Tests.GeneratorTests
             //All dates should not be the same
             Assert.IsFalse(dateTimes.All(x => x == dateTimes[0]));
         }
-
+        
         [Test(Description = "Can we generate datetimes within a couple-hour range?")]
         public void Can_Spawn_DateTimes_Within_Short_Range()
         {
@@ -49,6 +49,50 @@ namespace Faker.Tests.GeneratorTests
 
             //All dates should be less than today's date one year from now
             Assert.IsTrue(dateTimes.All(x => x <= DateTime.Now.AddYears(1)));
+
+            //All dates should not be the same
+            Assert.IsFalse(dateTimes.All(x => x == dateTimes[0]));
+        }
+
+        [Test(Description = "Can we generate random DateTimeOffsets?")]
+        public void Can_Create_DateTimeOffsets_Within_Range()
+        {
+            var dateTimes = new List<DateTimeOffset>();
+            for (var i = 0; i < 1000; i++)
+            {
+                dateTimes.Add(DateTimes.GetDateTimeOffset(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(1)));
+            }
+
+            //Should not have any dates below the current date
+            Assert.IsFalse(dateTimes.Any(x => x < DateTimeOffset.Now));
+
+            //All dates should be greater than the current date
+            Assert.IsTrue(dateTimes.All(x => x >= DateTimeOffset.Now));
+
+            //All dates should be less than today's date one year from now
+            Assert.IsTrue(dateTimes.All(x => x <= DateTimeOffset.Now.AddYears(1)));
+
+            //All dates should not be the same
+            Assert.IsFalse(dateTimes.All(x => x == dateTimes[0]));
+        }
+
+        [Test(Description = "Can we generate DateTimeOffsets within a couple-hour range?")]
+        public void Can_Spawn_DateTimeOffsets_Within_Short_Range()
+        {
+            var dateTimes = new List<DateTimeOffset>();
+            for (var i = 0; i < 1000; i++)
+            {
+                dateTimes.Add(DateTimes.GetDateTimeOffset(DateTimeOffset.Now, DateTimeOffset.Now.AddHours(2)));
+            }
+
+            //Should not have any dates below the current date
+            Assert.IsFalse(dateTimes.Any(x => x < DateTimeOffset.Now));
+
+            //All dates should be greater than the current date
+            Assert.IsTrue(dateTimes.All(x => x >= DateTimeOffset.Now));
+
+            //All dates should be less than today's date one year from now
+            Assert.IsTrue(dateTimes.All(x => x <= DateTimeOffset.Now.AddYears(1)));
 
             //All dates should not be the same
             Assert.IsFalse(dateTimes.All(x => x == dateTimes[0]));
