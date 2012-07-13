38e1fc0d448db76e969063dc67528e8a60049bef
Aaronontheweb <aaron@aaronstannard.com>
2012-07-12 22:16:13 -0700

    updated the assembly information of Faker for .NET 3.5; updated the Nuspec file and the self-packaging command to relfect the changes

--- package.cmd
+++ package.cmd
@@ -3,9 +3,11 @@ if not exist Download\packages mkdir Download\packages
 if not exist Download\packages\Faker mkdir Download\packages\Faker
 if not exist Download\packages\Faker\lib mkdir Download\packages\Faker\lib
 if not exist Download\packages\Faker\lib\net4 mkdir Download\packages\Faker\lib\net4
+if not exist Download\packages\Faker\lib\net4 mkdir Download\packages\Faker\lib\net35
 
 copy LICENSE.txt Download\packages\Faker
 
 copy Faker\bin\Release\Faker.dll Download\packages\Faker\lib\net4
+copy Faker.Net35\bin\Release\Faker.Net35.dll Download\packages\Faker\lib\net35
 
 nuget.exe pack Faker.nuspec -BasePath Download\packages\Faker -OutputDirectory Download
