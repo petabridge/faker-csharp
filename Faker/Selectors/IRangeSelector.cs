d5ac2628dad15777bf82ac864b6a760ba9661629
Aaron Stannard <aaron@aaronstannard.com>
2012-11-22 11:48:30 -0800

    Added an interface that allows for TypeSelectors to specify whether they support ranges or not

---
+++ Faker/Selectors/IRangeSelector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Faker.Selectors
+{
+    /// <summary>
+    /// Interface used for TypeSelectors that need to use ranges
+    /// </summary>
+    /// <typeparam name="T">The type we use to derive our maxs / mins</typeparam>
+    public interface IRangeSelector<T>
+    {
+        Func<T> Max { get; set; }
+        Func<T> Min { get; set; }
+    }
+}
