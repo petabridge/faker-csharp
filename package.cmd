98ee428dbd91eeae99f39adfb16d5973e11ee836
Aaron Stannard <aaron@markedup.com>
2014-05-02 13:13:55 -0700

    fixed build system for Faker 1.2

--- package.cmd
+++ package.cmd
@@ -13,4 +13,4 @@ msbuild "%CD%\Faker.sln" "/p:Configuration=Release;"
 copy %CD%\Faker\bin\Release\Faker.dll Download\packages\Faker\lib\net4
 copy %CD%\Faker.Net35\bin\Release\Faker.Net35.dll Download\packages\Faker\lib\net35
 
-nuget.exe pack Faker.nuspec -BasePath Download\packages\Faker -OutputDirectory Download
+%CD%\.nuget\NuGet.exe pack Faker.nuspec -BasePath Download\packages\Faker -OutputDirectory Download
