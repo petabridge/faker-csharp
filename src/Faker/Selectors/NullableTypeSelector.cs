26fbe7ce0425cfb5c36320fd8c4f2b0f501943ba
Aaron Stannard <aaron@petabridge.com>
2016-11-10 16:25:08 -0800

    verified that nullable selectors can work on nullable primitive types

--- src/Faker/Selectors/NullableTypeSelector.cs
+++ src/Faker/Selectors/NullableTypeSelector.cs
@@ -6,20 +6,36 @@ namespace Faker.Selectors
     /// <see cref="ITypeSelector"/> implementation designed to support
     /// nullability - the ability for null types to be represented as null.
     /// </summary>
-    public class NullableTypeSelector<T> : TypeSelectorBase<T> where T:new()
+    public class NullableTypeSelector<T> : TypeSelectorBase<T>
     {
-        private readonly float _nullProbability;
-        private readonly Func<T> _generatorFunc;
+        private readonly double _nullProbability;
+        private readonly Func<object> _generatorFunc;
 
-        public NullableTypeSelector(Func<T> generatorFunc, float nullProbability = SelectorConstants.DefaultNullProbability)
+        /// <summary>
+        /// Default constructor for nullable types.
+        /// </summary>
+        /// <param name="generatorFunc">A delegate capable of producing a type.</param>
+        /// <param name="nullProbability">The likelihood of generating a null versus a concrete value.</param>
+        public NullableTypeSelector(Func<object> generatorFunc, double nullProbability = SelectorConstants.DefaultNullProbability)
         {
             _nullProbability = nullProbability;
             _generatorFunc = generatorFunc;
         }
 
+        /// <summary>
+        /// Default constructor for nullable types.
+        /// </summary>
+        /// <param name="selector">A concrete type selector for the supported type.</param>
+        /// <param name="nullProbability">The likelihood of generating a null versus a concrete value.</param>
+        public NullableTypeSelector(ITypeSelector selector, double nullProbability = SelectorConstants.DefaultNullProbability)
+            : this(selector.GenerateInstance, nullProbability) { }
+
         public override T Generate()
         {
-            return Generators.Numbers.Float(0.0f, _nullProbability) <= _nullProbability ? default(T) : _generatorFunc();
+            var output = _generatorFunc();
+            if (Generators.Booleans.BoolWithProbability(_nullProbability))
+                return default(T);
+            return (T)output;
         }
     }
 }
---
+++ tests/Faker.Tests/SelectorTests/NullableSelectorTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Faker.Selectors;
+using NUnit.Framework;
+
+namespace Faker.Tests.SelectorTests
+{
+    [TestFixture]
+    public class NullableSelectorTests
+    {
+        #region Test classes
+
+        public class MyNullablePrimitiveTypeClass
+        {
+            public int? TestOne { get; set; }
+        }
+
+        #endregion
+
+        [Test]
+        public void NullableTypeSelector_supports_all_nullable_primivites()
+        {
+            var intSelector = new NullableTypeSelector<int?>(new IntSelector());
+            var c = new MyNullablePrimitiveTypeClass();
+
+            var ints = new List<int?>();
+
+            // The distribution of nulls over a large period of time should roughly
+            // be 1 in 10 by default, but the RNG may not distribute them evenly.
+            foreach (var property in c.GetType().GetProperties())
+            {
+                for (var i = 0; i < 100; i++)
+                {
+                    intSelector.Generate(c, property);
+                    ints.Add(c.TestOne);
+                    if (i % 10 == 0)
+                        Thread.Sleep(20);
+                }
+            }
+            
+            Assert.True(ints.Any(x => x == null));
+            Assert.True(ints.Any(x => x != null));
+        }
+    }
+}
